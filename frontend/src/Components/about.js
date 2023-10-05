d8ef4551141b4da22a70a5537a8909b97ec545bd
Donald646 <donaldchu901@gmail.com>
2023-10-04 22:57:40 -0500

    add package-lock.json

--- frontend/src/Components/about.js
+++ frontend/src/Components/about.js
@@ -3,7 +3,15 @@ import { React } from "react";
 export const AboutPage = () => {
   return (
     <>
-      <h1>About</h1>
+      <div className="about-page">
+        <div className="about-page1">
+          <h1>Made For Teachers</h1>
+          <h2>
+            Made By Donald Chu, a highschool student, who aims to make high
+            quality worksheets more accessible, and easier to make.
+          </h2>
+        </div>
+      </div>
     </>
   );
 };
--- frontend/src/Components/home.js
+++ frontend/src/Components/home.js
@@ -3,11 +3,12 @@ import React from "react";
 export const Home = () => {
   return (
     <div className="home-page">
-      <div className="home page1">
+      <div className="home-page1">
         <h1>Customized Worksheets, Generated With AI.</h1>
         <h2>Create Completely Customized Worksheets In Seconds</h2>
+        <h3>Generate Worksheets Like Magic</h3>
       </div>
-      <div className="home page2">
+      <div className="home-page2">
         <h1>Print or Save.</h1>
       </div>
     </div>
